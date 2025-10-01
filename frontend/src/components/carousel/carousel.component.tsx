ed45a9b5576f963e99f374dc70c5847d9a851f7a
ali18rin <alice.ringstrom@sundsvall.se>
2025-10-01 14:03:10 +0200

    fix: centered carousel items, scroll button size and aria labels

--- frontend/src/components/carousel/carousel.component.tsx
+++ frontend/src/components/carousel/carousel.component.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode, useRef } from 'react';
 import { Button, Icon, useThemeQueries } from '@sk-web-gui/react';
 import { ArrowLeft, ArrowRight } from 'lucide-react';
+import { useTranslation } from 'react-i18next';
 
 interface CarouselProps {
   children: ReactNode;
@@ -12,6 +13,7 @@ export const Carousel: React.FC<CarouselProps> = ({
   const scrollableRef = useRef<HTMLDivElement>(null);
   const { isMinMd } = useThemeQueries();
   const scrollAmount = isMinMd ? 450 : 300;
+  const { t } = useTranslation();
 
   const onScroll = (offSet: number) => {
     if (scrollableRef.current) {
@@ -20,14 +22,16 @@ export const Carousel: React.FC<CarouselProps> = ({
   };
 
   return (
-    <div className="w-full flex items-center">
+    <div className="w-full flex items-center justify-center">
       <Button
         className="absolute left-24 z-10"
+        size="lg"
         variant="tertiary"
         iconButton
         leftIcon={<Icon icon={<ArrowLeft />} />}
         rounded
         onClick={() => onScroll(-scrollAmount)}
+        aria-label={t('scenario:scrollLeft')}
       />
 
       <div
@@ -40,12 +44,14 @@ export const Carousel: React.FC<CarouselProps> = ({
       </div>
 
       <Button
-        className="absolute right-24"
+        className="absolute right-24 z-10"
+        size="lg"
         variant="tertiary"
         iconButton
         leftIcon={<Icon icon={<ArrowRight />} />}
         rounded
         onClick={() => onScroll(scrollAmount)}
+        aria-label={t('scenario:scrollRight')}
       />
     </div>
   );
