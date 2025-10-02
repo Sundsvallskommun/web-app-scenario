e16f6ed1b3c69aeb4e8a8b072b11c8f4aa6e9d94
ali18rin <alice.ringstrom@sundsvall.se>
2025-10-02 11:27:23 +0200

    fix: carousel gradient and button variant

--- frontend/src/components/carousel/carousel.component.tsx
+++ frontend/src/components/carousel/carousel.component.tsx
@@ -22,11 +22,13 @@ export const Carousel: React.FC<CarouselProps> = ({
   };
 
   return (
-    <div className="w-full flex items-center justify-center">
+    <div className="w-full flex items-center justify-center relative">
+      <div className="z-10 absolute blur-lg -left-72 w-[35rem] -top-24 -bottom-24 bg-gradient-to-r from-primitives-overlay-lighten-6 dark:from-primitives-overlay-darken-8 via-primitives-overlay-light-4 dark:via-primitives-overlay-darken-6 via-75% to-[rgba(255,255,255,0)] dark:to-[rgba(0,0,0,0)]"></div>
+      <div className="z-10 absolute blur-lg -right-72 w-[35rem] -top-24 -bottom-24 bg-gradient-to-l from-primitives-overlay-lighten-6 dark:from-primitives-overlay-darken-8 via-primitives-overlay-light-4 dark:via-primitives-overlay-darken-6 via-75% to-[rgba(255,255,255,0)] dark:to-[rgba(0,0,0,0)]"></div>
       <Button
         className="absolute left-24 z-10"
         size="lg"
-        variant="tertiary"
+        variant="primary"
         iconButton
         leftIcon={<Icon icon={<ArrowLeft />} />}
         rounded
@@ -46,7 +48,7 @@ export const Carousel: React.FC<CarouselProps> = ({
       <Button
         className="absolute right-24 z-10"
         size="lg"
-        variant="tertiary"
+        variant="primary"
         iconButton
         leftIcon={<Icon icon={<ArrowRight />} />}
         rounded
